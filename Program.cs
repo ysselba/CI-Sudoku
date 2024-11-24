29049986aab186680d157b0e1922892c9adc3160
Yss <144283482+ysselba@users.noreply.github.com>
2024-11-24 22:38:18 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+      // Main body
+    }
+  
+    public class Sudoku
+    {
+      // Klasse body
+    }
+}
