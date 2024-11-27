db17f516172e16ec637e61473e7282197a4bfe0e
Yss <144283482+ysselba@users.noreply.github.com>
2024-11-27 20:38:55 +0100

    Update Program.cs
    Code van vandaag

--- sudoku/sudoku/Program.cs
+++ sudoku/sudoku/Program.cs
@@ -1,35 +1,39 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Linq.Expressions;
 
-namespace sudoku
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        { // TEST
-            string[] tempInput = Console.ReadLine().Split(' ');
-            List<int> input = new List<int>();
-            foreach (string line in tempInput)
-            {
-                // Console.Write(line, int.Parse(line));
-                input.Add(int.Parse(line));
-            }
-            
-            //kan straks weg
-            if (input.Count != 81) Console.WriteLine("Verkeerd aantal cijfers");
-            else Console.WriteLine("parse gelukt");
-            
-            Hashtable ht = new Hashtable();
-            for (int i = 1; i <= 9; i++)
-            {
-                List<int> blok = new List<int>();
-                for (int n=0; n < 9; n++)
-                {
-                    blok.Add(input[n*(i)]);
-                }
-                ht.Add(i, blok);
-            }
+        string[] tempInput = Console.ReadLine().Split(' ');
+        List<int> input = new List<int>();
+        foreach (string line in tempInput)
+        {
+            // Console.Write(line, int.Parse(line));
+            input.Add(int.Parse(line));
         }
+        Hashtable ht = new Hashtable();
+        for (int i = 0; i < 9; i++)
+        {
+            List<int> blok = new List<int>();
+
+            for (int n=0; n < 9; n++)
+            {
+                blok.Add(input[n + (i*9)]);
+                Console.WriteLine($"{n}, {i}: {input[n + (i * 9)]}");
+            }
+
+            ht.Add(i, blok);
+        }
+        for(int i = 0; i < 9; i++)
+        {
+            Console.Write($"{i}: {ht[i]}");
+        }
+    }
+
+    public class Sudoku
+    {
+        // Klasse body
     }
 }
