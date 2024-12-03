b629b45bc844a0605f125b42d23e73a7c3b98b38
noei93wq20 <117650895+noei93wq20@users.noreply.github.com>
2024-12-03 09:09:16 +0100

    Comments toegevoegd

--- sudoku/sudoku/Program.cs
+++ sudoku/sudoku/Program.cs
@@ -11,21 +11,21 @@ public class Program
         List<SudokuCel> input = new List<SudokuCel>();
         for (int n = 0; n < tempInput.Length; n++)
         {
-            bool tempboolwaarde = false;
-            int grootblokx = GrootBlokCoord(n%9);
-            int grootbloky = GrootBlokCoord((int)Math.Floor(Convert.ToDecimal(n / 9)));
+            bool tempboolwaarde = false; // De temporary bool waarde van 'Gefixeerd' is eerst gewoon false
+            int grootblokx = GrootBlokCoord(n%9); // De x-coordinaat van het grote (3x3) blok waar de cel in is, deze x-coord is 0, 1 of 2
+            int grootbloky = GrootBlokCoord((int)Math.Floor(Convert.ToDecimal(n / 9))); // De y-coordinaat van het grote (3x3) blok waar de cel in is, deze y-coord is 0, 1 of 2
             SudokuCel nieuwecel = new SudokuCel(int.Parse(tempInput[n]), n % 9, (int)Math.Floor(Convert.ToDecimal(n / 9)), grootblokx, grootbloky, tempboolwaarde);
             if (int.Parse(tempInput[n]) != 0)
             {
-                nieuwecel.Gefixeerd = true;
+                nieuwecel.Gefixeerd = true; // Als de input op deze cel geen 0 is, is het gefixeerd
             }
             input.Add(nieuwecel);
         }
 
         static int GrootBlokCoord(int n)
-        {   return (int)Math.Floor(Convert.ToDecimal(n)) % 3;  }
+        {   return (int)Math.Floor(Convert.ToDecimal(n)) % 3;  // Simpele methode om te gebruiken voor het berekenen v/d coordinaten van de grote 3x3 blokken
         
-        Hashtable ht = new Hashtable();
+        Hashtable ht = new Hashtable(); // Deze hadden jullie (Yassin en Bram) erin gezet, moeten nog aangepast worden om te passen bij het nieuwe object
         for (int i = 0; i < 9; i++)
         {
             List<SudokuCel> blok = new List<SudokuCel>();
@@ -78,14 +78,14 @@ public class Program
         }
     }
 
-    public class SudokuCel
+    public class SudokuCel // Dit is het object SudokuCel dat ik gemaakt heb
     {
-        public int Waarde { get; set; }
-        public int Xcoord { get; set; }
-        public int Ycoord { get; set; }
-        public int GrootblokX { get; set; }
-        public int GrootblokY { get; set; }
-        public bool Gefixeerd { get; set; }
+        public int Waarde { get; set; } // De waarde van de cel (0, 1, 2, 3, 4, 5, 6, 7, 8 of 9)
+        public int Xcoord { get; set; } // De x-coordinaat van de locatie van de cel (0, 1, 2, 3, 4, 5, 6, 7 of 8)
+        public int Ycoord { get; set; } // De y-coordinaat van de locatie van de cel (0, 1, 2, 3, 4, 5, 6, 7 of 8)
+        public int GrootblokX { get; set; } // De x-coordinaat van het grote 3x3 blok waar de cel in zit (0, 1 of 2)
+        public int GrootblokY { get; set; } // De y-coordinaat van het grote 3x3 blok waar de cel in zit (0, 1 of 2)
+        public bool Gefixeerd { get; set; } // Boolwaarde die beschrijft of de waarde vast staat (waarde is geen 0) of niet (waarde is 0)
         public SudokuCel(int waarde, int xcoord, int ycoord, int grootblokx, int grootbloky, bool gefixeerd)
         { Waarde = waarde; Xcoord = xcoord; Ycoord = ycoord; GrootblokX = grootblokx; GrootblokY = grootbloky; Gefixeerd = gefixeerd; } 
     }
